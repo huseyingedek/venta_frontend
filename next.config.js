9998e3544a425f275666381d809d566451a056e5
Hüseyin <hgedek1881@gmail.com>
2026-06-27 00:47:36 +0300

    pp

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://api.ventapremium.com.tr/api/v1api/v1',
     NEXT_PUBLIC_SITE_NAME: 'Venta Premium',
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
   },
